b80193ccb13124f35860914961867c9be68f51d4
tanish <tanishchauhanmin@gmail.com>
2025-08-05 11:34:08 +0530

    all lifecycle hooks

--- src/app/chang-child/chang-child.component.ts
+++ src/app/chang-child/chang-child.component.ts
@@ -1,4 +1,4 @@
-import { Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-chang-child',
@@ -6,7 +6,36 @@ import { Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@an
   templateUrl: './chang-child.component.html',
   styleUrl: './chang-child.component.css'
 })
-export class ChangChildComponent implements OnChanges,OnInit,DoCheck {
+export class ChangChildComponent implements OnChanges,OnInit,DoCheck,AfterViewInit,AfterViewChecked,AfterContentInit,AfterContentChecked,OnDestroy{
+
+
+  timer:any;
+  ngOnDestroy(): void {
+    console.log("destroyed");
+    clearInterval(this.timer);
+  }
+  ngAfterContentChecked(): void {
+    console.log("content checked",this.fromParent);
+  }
+  ngAfterContentInit(): void {
+    console.log("content init",this.fromParent);
+  }
+  ngAfterViewChecked(): void {
+    console.log("view checked",this.hi)
+  }
+
+
+  @ViewChild('hi') hi!:ElementRef;
+
+
+  @ContentChild("fromParent") fromParent!:ElementRef;
+
+
+
+
+  ngAfterViewInit(): void {
+    console.log("after view init",this.hi);
+  }
   ngDoCheck(): void {
     console.log("ng do check in chald", this.dataObject);
   }
@@ -18,7 +47,19 @@ export class ChangChildComponent implements OnChanges,OnInit,DoCheck {
 
 
     console.log(" ng on onit in child",this.data)
+
+    console.log(this.hi);
+
+    this.timer = setInterval(()=>{
+      console.log("timer");
+    },1000)
     
+  }
+
+  onClick(){
+    console.log(this.hi);
+
+    this.hi.nativeElement.innerHTML="hello"
   }
 
 
--- src/app/change-detection/change-detection.component.ts
+++ src/app/change-detection/change-detection.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { ChangChildComponent } from "../chang-child/chang-child.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-change-detection',
-  imports: [ChangChildComponent],
+  imports: [ChangChildComponent,CommonModule],
   templateUrl: './change-detection.component.html',
   styleUrl: './change-detection.component.css'
 })
 export class ChangeDetectionComponent {
 
   childData:string = "data from parent"
+
+  show:boolean=true;
 
 
   childObject ={
