7535b622237095ab78def47c814b6b07619f0927
tanish <tanishchauhanmin@gmail.com>
2025-08-08 14:35:17 +0530

    subjects

--- src/app/rxjs/index-component/index-component.component.ts
+++ src/app/rxjs/index-component/index-component.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { debounceTime, filter, interval, map, Observable, of, Subscription, take } from 'rxjs';
+import { BehaviorSubject, catchError, debounceTime, filter, interval, map, Observable, of, ReplaySubject, retry, Subject, Subscription, take } from 'rxjs';
 
 @Component({
   selector: 'app-index-component',
@@ -11,62 +11,140 @@ import { debounceTime, filter, interval, map, Observable, of, Subscription, take
 export class IndexComponentComponent implements OnInit,OnDestroy{
   ngOnDestroy(): void {
     console.log("destroyed")
-    this.subOb.unsubscribe();
+    // this.subOb.unsubscribe();
   }
   
-  myObservable = new Observable<string>((observer)=>{
-    observer.next("hi")
+  // myObservable = new Observable<string>((observer)=>{
+  //   observer.next("hi")
     
-    observer.next("hello")
+  //   observer.next("hello")
 
-    observer.complete();
+  //   observer.complete();
 
-  });
-
-
-  form!:FormGroup;
+  // });
 
 
+  // form!:FormGroup;
 
-  subOb!:Subscription;
+
+
+  // subOb!:Subscription;
+
+
+
+  sub = new ReplaySubject<string>(3);
+
+  attemps:number = 1;
+
+  fakeApi(){
+
+
+    console.log(this.attemps);
+
+
+    this.attemps = this.attemps +1;
+
+
+
+
+    let obs = new Observable<string>((observer)=>{
+      // observer.next("hi");
+
+
+      if(this.attemps >2){
+observer.next("heelo")
+      }else{
+// observer.error("kjdsng")
+observer.next("hjads");
+      }
+
+
+      
+      
+    });
+    return obs;
+  }
+
+
+
+  unsub!:Subscription;
+
 
 
 
 
   ngOnInit(): void {
 
-    of(1,2,3,4,5).pipe(
-      map((data)=>data*5),
-      filter((x)=>x%2 === 0)
-    ).subscribe((data)=>console.log(data));
+    // of(1,2,3,4,5).pipe(
+    //   map((data)=>data*5),
+    //   filter((x)=>x%2 === 0)
+    // ).subscribe((data)=>console.log(data));
 
 
-    let obser = new Observable((subsriber)=>{
-      subsriber.next("appple")
-      subsriber.next("grapes")
-    })
+    // let obser = new Observable((subsriber)=>{
+    //   subsriber.next("appple")
+    //   subsriber.next("grapes")
+    // })
 
-    let modify = obser.pipe(
-      map((x,index)=>`${index} --- ${x}`)
-    )
+    // let modify = obser.pipe(
+    //   map((x,index)=>`${index} --- ${x}`)
+    // )
 
-    modify.subscribe((data)=>console.log(data));
-    obser.subscribe((x)=>console.log(x));
+    // modify.subscribe((data)=>console.log(data));
+    // obser.subscribe((x)=>console.log(x));
 
-    this.form = new FormGroup({
-      search:new FormControl("")
-    })
+    // this.form = new FormGroup({
+    //   search:new FormControl("")
+    // })
+
+
+
+    // this.fakeApi().pipe(
+    //     retry(5),
+
+    //     catchError(err => {
+    //   // this.errorMessage = 'Failed after 3 attempts';
+    //   return of([]);
+    // })
+
+    // )
+    // .subscribe((value)=>console.log(value));
+this.sub.next("hi")
+this.unsub = this.sub.subscribe((value)=>console.log(value));
+
+this.sub.next("what");
+
+    
+
+    const a = this.sub.subscribe((value)=>console.log(value));
+    const b = this.sub.subscribe((value)=>console.log(value));
+
+
+this.unsub.add(a);
+this.unsub.add(b);
+
+
+    
   }
 
 
-  onSubmit(){
-    this.form.get('search')?.valueChanges.pipe(debounceTime(2000)).subscribe((x)=>console.log(x));
+
+  onCLick(){
+
+    // this.unsub.unsubscribe();
+
+    this.sub.next("clicked");
   }
+
+
+  // onSubmit(){
+  //   this.form.get('search')?.valueChanges.pipe(debounceTime(2000)).subscribe((x)=>console.log(x));
+  // }
 
 
  
 
 
-  timer$ = interval(1000); 
-  fruit$ = of("appple","banana");
+  // timer$ = interval(1000); 
+  // fruit$ = of("appple","banana");
 }
