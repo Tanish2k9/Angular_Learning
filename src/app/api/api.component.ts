254830ae7c8d82d0db5faab1a49b482febbbf573
tanish <tanishchauhanmin@gmail.com>
2025-08-13 11:27:47 +0530

    http request option headers params etc

--- src/app/api/api.component.ts
+++ src/app/api/api.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
@@ -13,17 +13,34 @@ export class ApiComponent implements OnInit {
   posts:any[]=[];
   
   ngOnInit(): void {
-    this.getPost().subscribe((data:any)=>{this.posts = data})
+    localStorage.setItem('token',"asjfkajdskfjkanskfjkladsjfklj");
+    
+    this.getPost().subscribe((data:any)=>{
+      if(data.ok){
+        this.posts = data.body;
+      }
+      if(data.status === 401){
+        alert("you can t access this page")
+      }
+      console.log("------------",data)
+      })
   }
   http = inject(HttpClient);
 
   getPost(){
-    return this.http.get("http://localhost:3000/posts");
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization':`Bearer ${token}`
+    })
+
+    const params = new HttpParams().set('page','1').set('sort','asc');
+
+    return this.http.get("http://localhost:3000/posts",{headers:headers,params:params,observe:'response',responseType:'json'});
   }
 
 
   onSubmit(){
-    let idn = this.posts?.length + 1;
+    let idn = this.posts.length + 1;
 
     const id = idn.toString();
 
@@ -31,9 +48,10 @@ export class ApiComponent implements OnInit {
 
     this.http.post("http://localhost:3000/posts",payload).subscribe((data)=>{
       console.log(data);
+      this.getPost().subscribe((data:any)=>this.posts = data.body);
     })
 
-    this.getPost().subscribe((data:any)=>this.posts = data);
+    
   }
 
 }
